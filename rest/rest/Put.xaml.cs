30ac2af9b68bd4ec2871f9d2b32515f33ff4bc0a
ERMES_DMS <ERMES_DMS@PC>
2020-11-18 23:17:45 -0500

    solutions

--- rest/rest/Put.xaml.cs
+++ rest/rest/Put.xaml.cs
@@ -25,23 +25,23 @@ namespace rest
 
             await Navigation.PushAsync(new MainPage());
 
-           
+    
         }
 
         
         private async void btnActualizar_Clicked(object sender, EventArgs e)
         {
-        
+            
+            
             string Url = "http://192.168.1.12/Rest/post.php";
             HttpClient httpClient = new HttpClient();
             var builder = new UriBuilder(Url);
-            builder.Port = -1;
             var query = HttpUtility.ParseQueryString(builder.Query);
 
             query["codigo"] = codigo.Text;
-            query["nombre"] = txtActualizar_nombre.Text ;
+            query["nombre"] = txtActualizar_nombre.Text;
             query["apellido"] = txtActualizar_apellido.Text;
-            query["edad"] = txtActualizar_edad.Text;
+            query["edad"] =txtActualizar_edad.Text;
 
             builder.Query = query.ToString();
             string url = builder.ToString();
@@ -49,9 +49,10 @@ namespace rest
 
             var consultaSerializada = await httpClient.PutAsync(url, null);
             await App.Current.MainPage.DisplayAlert("Alerta", "Dato Ingresado correctamente", "ok");
-          
+         
 
 
+            
         }
 
     }
