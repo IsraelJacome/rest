a42c0970670b6d041eead37da94b013213a90c22
ERMES_DMS <ERMES_DMS@PC>
2020-11-18 22:16:26 -0500

    Agregar archivos de proyecto.

---
+++ rest/rest/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace rest
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private const string Url = "http://192.168.1.12/Rest/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<rest.Datos> _post;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            get();
+        }
+
+        public async void get()
+        {
+            try
+            {
+                var content = await client.GetStringAsync(Url);
+                List<rest.Datos> posts = JsonConvert.DeserializeObject<List<rest.Datos>>(content);
+                _post = new ObservableCollection<rest.Datos>(posts);
+
+                MyListView.ItemsSource = _post;
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", "ERROR " + ex.Message, "OK");
+            }
+        }
+
+
+        private async void btnGet_Clicked(object sender, EventArgs e)
+        {
+            
+                await Navigation.PushAsync(new InsertarDato());
+                //  await Navigation.PopAsync();
+
+
+        }
+
+        private async void btnDelete_Clicked(object sender, EventArgs e)
+        {
+
+            await Navigation.PushAsync(new Delete());
+
+
+        }
+
+        private async void btnPut_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Put());
+        }
+    }
+}
---
+++ rest/rest/Put.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Net;
+using System.Net.Http;
+
+
+namespace rest
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Put : ContentPage
+    {
+        public Put()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnRegresar_Clicked(object sender, EventArgs e)
+        {
+
+            await Navigation.PushAsync(new MainPage());
+
+            /*  try
+              {
+                  var url = "http://192.168.1.12/Rest/post.php";
+                  HttpClient cliente = new HttpClient();
+                  var id = txtActualizar_id.Text;
+                  var nombre = txtActualizar_nombre.Text;
+                  var apellido = txtActualizar_apellido.Text;
+                  var edad = txtActualizar_edad.Text;
+
+                  var uri = new Uri(string.Format(url + "?codigo=" + id+"&"+"?nombre="+nombre+"?apellido="+apellido));
+
+                  var response = await cliente.DeleteAsync(uri);
+                  if (response.IsSuccessStatusCode)
+                  {
+                      await DisplayAlert("alerta", "Actualizado correctamente: ", "ok");
+
+                  }
+                  else
+                  {
+                      await DisplayAlert("alerta", "ERROR:: ", "ok");
+                  }
+
+              }
+              catch (Exception ex)
+              {
+                  await DisplayAlert("alerta", "Error: " + ex.Message, "ok");
+              }
+            */
+        }
+
+        
+        private async void btnActualizar_Clicked(object sender, EventArgs e)
+        {
+            
+            /*
+            string Url = "http://192.168.1.12/Rest/post.php";
+            HttpClient httpClient = new HttpClient();
+            var builder = new UriBuilder(Url);
+            builder.Port = -1;
+            var query = HttpUtility.ParseQueryString(builder.Query);
+
+            query["codigo"] = this.modelo.codigo.ToString();
+            query["nombre"] = this.modelo.nombre;
+            query["apellido"] = this.modelo.apellido;
+            query["edad"] = this.modelo.edad;
+
+            builder.Query = query.ToString();
+            string url = builder.ToString();
+
+
+            var consultaSerializada = await httpClient.PutAsync(url, null);
+            await App.Current.MainPage.DisplayAlert("Alerta", "Dato Ingresado correctamente", "ok");
+            await Application.Current.MainPage.Navigation.PopAsync();
+
+
+            */
+        }
+
+    }
+}
